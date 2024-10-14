eb3087529aa42ed5b1c29aa08113cf381bac3f39
yuukiyokoyama <yuukitigars89@gmail.com>
2024-10-14 13:50:05 +0900

    ログイン画面のルーティング設定

--- music-app/src/App.jsx
+++ music-app/src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
-function App() {
-
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <BrowserRouter>
+      <div className="App">
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
